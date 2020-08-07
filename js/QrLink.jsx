78e394484f92436a54d6fb9e30276e20596515d3
Martin Gude <m@martin-gude.com>
2020-08-07 18:52:58 +0200

    Added tmbx.li short link

--- js/QrLink.jsx
+++ js/QrLink.jsx
@@ -1,40 +1,63 @@
 'use strict';
 
 class QrLink extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { sharingEnabled: false };
-    
-    this.toggleSharing = this.toggleSharing.bind(this);
-  }
-  
-  toggleSharing() {
-    this.setState(state => ({
-      sharingEnabled: !state.sharingEnabled
-    }));
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            sharingEnabled: false
+        };
 
-  render() {
-		const qrserver = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&format=svg&data=';
-		const watchURI = window.location.origin + "/watch#" + this.props.channelId;
-	 	const qrcode = qrserver + encodeURIComponent(watchURI);
-    
-    let sharingLinks = "";
-    if (this.state.sharingEnabled) {
-      sharingLinks = <p>
-        Watch this timer on <a target="_blank" href={watchURI}>{watchURI}</a>.
-        <br/><br/>
-        <img src={qrcode} />
-      </p>;
+        this.toggleSharing = this.toggleSharing.bind(this);
     }
 
-    return (
-      <div>
-        <label htmlFor="enable-sharing" className="pure-checkbox">
-        <input type="checkbox" id="enable-sharing" onChange={this.toggleSharing} checked={this.state.sharingEnabled}/> Enable Sharing</label>
-        {sharingLinks}
-      </div>
-    );
-  }
-}
+    toggleSharing() {
+        this.setState(state => ({
+            sharingEnabled: !state.sharingEnabled
+        }));
+    }
 
+    render() {
+        const qrserver = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&format=svg&data=';
+        // const watchURI = window.location.origin + "/watch#" + this.props.channelId;
+        const watchURI = "https:///tmbx.li/" + this.props.channelId;
+        const qrcode = qrserver + encodeURIComponent(watchURI);
+
+        let sharingLinks = "";
+        if (this.state.sharingEnabled) {
+            sharingLinks = < p >
+                Watch this timer on < a target = "_blank"
+            href = {
+                    watchURI
+                } > {
+                    watchURI
+                } < /a>. <
+                br / > < br / >
+                <
+                img src = {
+                    qrcode
+                }
+            /> <
+            /p>;
+        }
+
+        return ( <
+            div >
+            <
+            label htmlFor = "enable-sharing"
+            className = "pure-checkbox" >
+            <
+            input type = "checkbox"
+            id = "enable-sharing"
+            onChange = {
+                this.toggleSharing
+            }
+            checked = {
+                this.state.sharingEnabled
+            }
+            /> Enable Sharing</label > {
+                sharingLinks
+            } <
+            /div>
+        );
+    }
+}
